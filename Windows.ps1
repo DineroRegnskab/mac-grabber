0bfa85154116808de6b0bfef2ccccee1930c9ffc
Søren Hansen <soren@dinero.dk>
2021-08-27 12:38:40 +0200

    Added script to grab from Windows.

---
+++ Windows.ps1
@@ -0,0 +1,35 @@
+$Lan = $null;
+$WiFi = $null;
+
+Get-NetAdapter -Physical | % {
+
+    if ($_.Name.StartsWith("Ethernet") -and $Lan -eq $null)
+    {
+        $Lan = $_.MacAddress;
+    }
+
+    if ($_.Name -eq "Wi-Fi" -and $WiFi -eq $null)
+    {
+        $WiFi = $_.MacAddress;
+    }
+}
+
+if ($Lan -eq $null)
+{
+    "✖ LAN adresse ikke fundet. Forbind din PC via et kabel."
+    return;
+}
+else
+{
+    "✔ LAN adresse fundet: ${Lan}"
+}
+
+if ($WiFi -eq $null)
+{
+    "✖ Wi-Fi adresse ikke fundet. Forbind din PC til et Wi-Fi."
+    return;
+}
+else
+{
+    "✔ Wi-Fi adresse fundet: ${WiFi}"
+}
